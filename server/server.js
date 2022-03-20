31eee1a0e58bca9173c206d65c6768a6158653cb
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-03-19 22:50:55 -0400

    Second push
    Added the server side

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//Constants
+
+const express = require(`express`);
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(`../client/dist`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require(`./routes/htmlRoutes`)(app);
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
